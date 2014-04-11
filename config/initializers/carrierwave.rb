eb367a23b2bf1ca86a73c368669d9527bc7245b4
David Yip <dvd.yip@gmail.com>
2014-04-11 10:03:17 -0400

    tinkering with initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['S3_KEY'],                # required
     :aws_secret_access_key  => ENV['S3_SECRET'],             # required
-    #:region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :region                 => 'us-east-1',                  # optional, defaults to 'us-east-1'
     #:host                   => 's3.example.com',             # optional, defaults to nil
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
